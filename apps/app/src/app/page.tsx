8c629f98b107d755e6f4aef1739ba4884f04a860
main <plvdev@proton.me>
2025-07-12 14:34:30 +0200

    feat(home): enhance HomePage with dynamic metadata and search parameter handling
    - Introduced dynamic metadata generation for the HomePage component.
    - Refactored Home component to handle search parameters and display relevant content based on the presence of the "p" parameter.
    - Updated the export to use a higher-order component for parameter handling.

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,11 +1,25 @@
-export default function Home() {
+import { type NextPageProps, withParams } from '@/lib/wrappers/with-params';
+import type { Metadata } from 'next';
+
+export const dynamic = 'force-dynamic';
+
+export const generateMetadata = (): Metadata => ({
+  title: 'Home',
+  description: 'Welcome to Raliz',
+});
+
+async function HomePage({ searchParams }: NextPageProps) {
+  const p = (await searchParams)?.p;
+
   return (
     <section>
       <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus id eaque maxime nihil harum eligendi
-        nisi nostrum perferendis neque quasi obcaecati temporibus ullam, quod nemo ad fuga molestias voluptatem
-        voluptate.
+        {p
+          ? `Search param "p" value: ${p}`
+          : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus id eaque maxime nihil harum eligendi nisi nostrum perferendis neque quasi obcaecati temporibus ullam, quod nemo ad fuga molestias voluptatem voluptate.'}
       </div>
     </section>
   );
 }
+
+export default withParams(HomePage);
