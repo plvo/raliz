d7ee53f909c06286505d7d5f56be402876d7d81a
main <plvdev@proton.me>
2025-07-12 14:17:49 +0200

    feat(backoffice): update environment variables and enhance client component
    - Added new environment variables for Web3Auth and contract addresses in .env.example.
    - Enhanced ClientIndex component with WalletButton and improved user data display.
    - Removed obsolete login form component to streamline authentication process.

--- apps/backoffice/src/app/client.tsx
+++ apps/backoffice/src/app/client.tsx
@@ -1,14 +1,16 @@
 'use client';
 
+import { WalletButton } from '@/components/shared/wallet-button';
 import { useUser } from '@/lib/providers/user-provider';
 
 export default function ClientIndex() {
   const useUserData = useUser();
 
   return (
-    <div>
+    <div className='flex flex-col gap-4'>
       <h1>Client Index</h1>
-      <pre>{JSON.stringify(useUserData, null, 2)}</pre>
+      <WalletButton />
+      <pre>{JSON.stringify({ useUserData: { ...useUserData, balance: useUserData.balance?.formatted } }, null, 2)}</pre>
     </div>
   );
 }
